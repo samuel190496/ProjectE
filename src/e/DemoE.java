af57b98e256c8645c662c7f582b1073d9398cd06
Samuel <Samuel@192.168.0.21>
2024-01-03 18:10:11 -0800

    initial commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,12 @@
+package e;
+
+public class DemoE {
+	public static void main(String[] args) {
+		System.out.println("Friday");
+		
+		System.out.println("Git hub set up");
+		System.out.println("Fahim is doing okey ");
+	}
+	
+
+}
